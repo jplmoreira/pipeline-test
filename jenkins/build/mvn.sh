4806ffb7f1bcd219fe251bcd533daa4d17ca4fed
jplmoreira <jplmoreira92@gmail.com>
2020-06-18 17:38:03 +0100

    Fix script workspace path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "********************"
 echo "*** Building jar ***"
 echo "********************"
 
-docker run --rm -v $PWD/java-app:/app -v $PWD/.m2:/root/.m2 -w /app maven:3-alpine mvn "$@"
+WORKSPACE=/Users/jplmoreira/Documents/Projects/jenkins/jenkins_home/workspace/pipeline
+
+docker run --rm -v $WORKSPACE/java-app:/app -v $WORKSPACE/.m2:/root/.m2 -w /app maven:3-alpine mvn "$@"
