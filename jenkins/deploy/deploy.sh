4a3d52a783d6c5695220d867746674eb3ded1a6a
jplmoreira <jplmoreira92@gmail.com>
2020-06-18 18:12:48 +0100

    Add post actions to Jenkinsfile

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@
 echo 'maven-project' > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 
-scp -i ~/Downloads/aws.pem /tmp/.auth ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com:/tmp/.auth
-scp -i ~/Downloads/aws.pem ./jenkins/deploy/publish.sh ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com:/tmp/publish.sh
-ssh -i ~/Downloads/aws.pem ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com "/tmp/publish.sh"
+scp -i /opt/aws.pem /tmp/.auth ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com:/tmp/.auth
+scp -i /opt/aws.pem ./jenkins/deploy/publish.sh ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com:/tmp/publish.sh
+ssh -i /opt/aws.pem ec2-user@ec2-18-132-60-68.eu-west-2.compute.amazonaws.com "/tmp/publish.sh"
